275fbc2f14005256c11f86352224c85244849af8
Zeynab Yad <zeynab.design@gmail.com>
2022-06-30 00:42:44 +0430

    Add AddListener to Seat Icon.

--- js/index.js
+++ js/index.js
@@ -3,77 +3,84 @@ const rows = [
   {
     rowNumber: 0,
     seats: [
-      { seatNumber: 1, SeatState: "available" },
-      { seatNumber: 2, SeatState: "reserved" },
-      { seatNumber: 3, SeatState: "available" },
+      { seatNumber: 1, seatState: "available" },
+      { seatNumber: 2, seatState: "reserved" },
+      { seatNumber: 3, seatState: "available" },
     ],
   },
   {
     rowNumber: 1,
     seats: [
-      { seatNumber: 1, SeatState: "available" },
-      { seatNumber: 2, SeatState: "reserved" },
-      { seatNumber: 3, SeatState: "available" },
+      { seatNumber: 1, seatState: "available" },
+      { seatNumber: 2, seatState: "available" },
+      { seatNumber: 3, seatState: "available" },
     ],
   },
   {
     rowNumber: 2,
     seats: [
-      { seatNumber: 1, SeatState: "available" },
-      { seatNumber: 2, SeatState: "reserved" },
-      { seatNumber: 3, SeatState: "available" },
+      { seatNumber: 1, seatState: "available" },
+      { seatNumber: 2, seatState: "available" },
+      { seatNumber: 3, seatState: "available" },
     ],
   },
   {
     rowNumber: 3,
     seats: [
-      { seatNumber: 1, SeatState: "available" },
-      { seatNumber: 2, SeatState: "reserved" },
-      { seatNumber: 3, SeatState: "available" },
+      { seatNumber: 1, seatState: "available" },
+      { seatNumber: 2, seatState: "available" },
+      { seatNumber: 3, seatState: "available" },
     ],
   },
   {
     rowNumber: 4,
     seats: [
-      { seatNumber: 1, SeatState: "available" },
-      { seatNumber: 2, SeatState: "reserved" },
-      { seatNumber: 3, SeatState: "available" },
+      { seatNumber: 1, seatState: "available" },
+      { seatNumber: 2, seatState: "reserved" },
+      { seatNumber: 3, seatState: "reserved" },
     ],
   },
   {
     rowNumber: 5,
     seats: [
-      { seatNumber: 1, SeatState: "available" },
-      { seatNumber: 2, SeatState: "reserved" },
-      { seatNumber: 3, SeatState: "available" },
+      { seatNumber: 1, seatState: "available" },
+      { seatNumber: 2, seatState: "available" },
+      { seatNumber: 3, seatState: "available" },
     ],
   },
   {
     rowNumber: 6,
     seats: [
-      { seatNumber: 1, SeatState: "available" },
-      { seatNumber: 2, SeatState: "reserved" },
-      { seatNumber: 3, SeatState: "available" },
+      { seatNumber: 1, seatState: "available" },
+      { seatNumber: 2, seatState: "available" },
+      { seatNumber: 3, seatState: "available" },
     ],
   },
 ];
 
 function drawSeats() {
-  let result = rows.map((row)=>
- `<tr>
-  <td><span ><i id='${
-    row.seats[0].seatNumber + row.rowNumber * 3
-  }' class="fa-solid fa-square" ></i></span></td>
+  let result = rows.map((row) =>
+    `<tr>
+  <td><span ><i id='${row.seats[0].seatNumber + row.rowNumber * 3
+    }' class="fa-solid fa-square ${row.seats[0].seatState}" ></i></span></td>
   <td>
-       <span class="list-inline-item"><i id='${
-         row.seats[1].seatNumber + row.rowNumber * 3
-       }' class="fa-solid fa-square" ></i></span>
-       <span class="list-inline-item"><i id='${
-         row.seats[2].seatNumber + row.rowNumber * 3
-       }' class="fa-solid fa-square"></i></span>
+       <span class="list-inline-item"><i id='${row.seats[1].seatNumber + row.rowNumber * 3
+    }' class="fa-solid fa-square ${row.seats[1].seatState}" ></i></span>
+       <span class="list-inline-item"><i id='${row.seats[2].seatNumber + row.rowNumber * 3
+    }' class="fa-solid fa-square ${row.seats[2].seatState}"></i></span>
   </td>
 </tr>`);
 
-document.querySelector('tbody').innerHTML=result.join('');
+  document.querySelector('tbody').innerHTML = result.join('');
+  addListener()
 }
 
+function addListener() {
+  let icons = document.querySelectorAll('tbody i');
+  for (let icon of icons) {
+    icon.addEventListener('click', checkStatus)
+  }
+}
+function checkStatus() {
+
+}
